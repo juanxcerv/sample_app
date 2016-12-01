531ecdac259403952c92d7d63aa7ac07f17b3a49
Juan Cervantes <juanxcerv@berkeley.edu>
2016-12-01 03:15:19 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  root              'static_pages#home'
 
-  get 'static_pages/about'
+  get 'help'    =>  'static_pages#help'
+
+  get 'about'   =>  'static_pages#about'
   
-  get 'static_pages/contact'
+  get 'contact' =>  'static_pages#contact'
+  
+  get 'signup'  =>  'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+
 end
